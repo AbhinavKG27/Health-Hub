3dc8f66cafe17b40ed4a9cbfd2e0c04d4eed555a
Verma116 <157568967+AbhinavKG27@users.noreply.github.com>
2025-09-21 18:11:47 +0530

    ok work

---
+++ frontend/src/Components/User/pages/Contact.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+} from '@mui/material';
+import { Email, Phone, LocationOn } from '@mui/icons-material';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import "./contact.css"
+
+const ContactUsPage = () => {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+      contact,
+    };
+    const response = await axios.post('http://localhost:8080/patient/patientmessage', data);
+    if (response.status === 200) {
+      toast.success(response.data.message);
+      navigate("/");
+
+    } else {
+      toast.error(response.data.message);
+
+    }
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [contact, setContact] = useState("");
+
+
+  return (
+    <Box py={4} sx={{ backgroundColor: '#ced3db' }}>
+      <Container maxWidth="md">
+        <Typography variant="h4" align="center" gutterBottom>
+          Contact Us
+        </Typography>
+
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} sm={6}>
+            <Box display="flex" alignItems="center">
+              <Email sx={{ mr: 1 }} />
+              <Typography variant="body1" id="changecolor">
+                Email: chiranth@gmail.com
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Box display="flex" alignItems="center">
+              <Phone sx={{ mr: 1 }} />
+              <Typography variant="body1" id="changecolor" >
+                Phone: 9986278942
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Address
+          </Typography>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} sm={6}>
+              <Box display="flex" alignItems="center">
+                <LocationOn sx={{ mr: 1 }} />
+                <Typography variant="body1" class="changecolor" id="changecolor">
+                  Karnataka
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Box display="flex" alignItems="center">
+                <LocationOn sx={{ mr: 1 }} />
+                <Typography variant="body1" id="changecolor">
+                Mandya
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Map
+          </Typography>
+          <Box height={400} mt={2}>
+            {/* <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Baneshwor+(Bharosha%20Hospital)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population mapping</a></iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3894.8042661374684!2d76.89177717615401!3d12.529122192857372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bafa1206a1427f1%3A0xf5457df9790f2966!2sMandya!5e0!3m2!1sen!2sin!4v1751019929063!5m2!1sen!2sin"  width="850" height="400"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </Box>
+        </Box>
+
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Contact Form
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Contact Number"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={contact}
+                  onChange={(e) => setContact(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Message"
+                  variant="outlined"
+                  multiline
+                  rows={4}
+                  fullWidth
+                  required
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" color="primary" type="submit">
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default ContactUsPage;
